9560e168acfb5750eea62ed66dd09fb3b8237908
Gary Tyler <tylermcleod@gmail.com>
2016-03-22 23:21:12 -0700

    readded export to Cinplane_Output dir

---
+++ Assets/Cineplane/Scripts/AlembicExporterNameUpdate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class AlembicExporterNameUpdate : MonoBehaviour
+{
+
+    AlembicExporter ae;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        ae = GetComponent<AlembicExporter>();
+        if (!Directory.Exists("Cineplane_Output/"))
+        {
+            Directory.CreateDirectory("Cineplane_Output/");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ae.m_outputPath = "Cineplane_Output/" + System.DateTime.Now.ToString("MM-dd_HH-mm-ss") + ".abc";
+    }
+}
